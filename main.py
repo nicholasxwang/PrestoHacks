9da35485d55d365ec9fcf54ffbd96ca1c4bb66da
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-12-11 01:51:09 +0000

    even more updates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Imports
 from flask import Flask, render_template
+from waitress import serve
 
 #Inits
 app = Flask('app')
@@ -8,6 +9,13 @@ app = Flask('app')
 @app.route('/')
 def main():
   return render_template('index.html')
+@app.route('/login')
+def login():
+  return render_template('login.html')
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
 
+#Requests
 #Run
 app.run(host='0.0.0.0', port=8080)
