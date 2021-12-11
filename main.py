1508fae01dd63e531243237d001b5f1f7164a7c1
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-12-11 02:06:57 +0000

    update

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 #Imports
 from flask import Flask, render_template
 from waitress import serve
+import os
+from flask_mail import Mail, Message
+
 
 #Inits
 app = Flask('app')
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = '@gmail.com'
+app.config['MAIL_PASSWORD'] = os.getenv('psw')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
 
 #Routing
 @app.route('/')
@@ -18,4 +28,4 @@ def signup():
 
 #Requests
 #Run
-app.run(host='0.0.0.0', port=8080)
+serve(app, host="0.0.0.0", port=8080)
